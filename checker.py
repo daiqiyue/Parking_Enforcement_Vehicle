1d87f9f1c7efc5f79edbde9bd3f83c28a8e85912
dai230 <daiqiyue2003@gmail.com>
2025-04-04 11:59:47 -0400

    update about database

---
+++ checker.py
@@ -0,0 +1,26 @@
+import mysql.connector
+from mysql.connector import errorcode
+def main():
+    plate_num = 'QD7777'
+    try:
+        cnx = mysql.connector.connect(user = 'root', password = 'Dqy20030729@', host = 'localhost', database = "license_plate")
+        print("Successfully connected to the database")
+        cursor = cnx.cursor()
+        query = ("select * from plate_info")
+        cursor.execute(query)
+        print(cursor)
+        for element in cursor:
+            print(f"element is: {element}")
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("The database doesn't exist")
+    
+
+
+    cnx.close()
+
+if __name__ == "__main__":
+    main()
